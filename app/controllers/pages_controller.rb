648b42e69f7f5dd59e98376b715e81f19c0bf292
Jonathan Serafini <jonathan.serafini@gmail.com>
2017-02-26 18:35:39 +0100

    modifs of last minutes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,14 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @hash = {}
+    addresses = [ { latitude: 44.835932, longitude: -0.572410},
+                  { latitude: 45.473553, longitude: -1.096063},
+                  { latitude: 44.614637, longitude: 0.662878}
+                ]
+    @hash = Gmaps4rails.build_markers(addresses) do |address, marker|
+      marker.lat address[:latitude]
+      marker.lng address[:longitude]
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+    end
   end
 end
